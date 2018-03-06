48248d3d6972bda9b8546892f3e55090fe737194
William Cheng <willchengsters@gmail.com>
2018-03-06 11:21:18 -0800

    better switching between profiles

--- slidingProfile/main.js
+++ slidingProfile/main.js
@@ -15,19 +15,14 @@ function applyEventHandlers(){
 }
 
 function profile1Animation(){
-    $('.picture').toggleClass('profilePic')
-    $('.picture').css('background-image','url(https://static.zerochan.net/Ryuu.%28Street.Fighter%29.full.324542.jpg)')
+    $('.prof1').toggleClass('profilePic')
 }
 function profile2Animation(){
-    $('.picture').toggleClass('profilePic')
-    $('.picture').css('background-image','url(http://vignette4.wikia.nocookie.net/streetfighter/images/b/be/Akuma_sf4.jpg/revision/latest?cb=20091022184740)')
+    $('.prof2').toggleClass('profilePic')
 }
 function profile3Animation(){
-    $('.picture').toggleClass('profilePic')
-    $('.picture').css('background-image','url(https://i.pinimg.com/736x/22/1f/a5/221fa5960065414c994938ebd655e1ce--ken-street-fighter-street-fighter-characters.jpg)')
+    $('.prof3').toggleClass('profilePic')
 }
 function profile4Animation(){
-    $('.picture').toggleClass('profilePic')
-    $('.picture').css('background-image','url(http://www.writeups.org/wp-content/uploads/Sakura-Kasugano-Street-Fighters-d.jpg)')
-
+    $('.prof4').toggleClass('profilePic')
 }
