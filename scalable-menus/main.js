3a70a26b1243cfc93a436382c0feabf2969e64ae
Will Cheng <willchengsters@gmail.com>
2018-03-15 10:56:15 -0700

    trying to get cursor position more accurate

--- scalable-menus/main.js
+++ scalable-menus/main.js
@@ -1,37 +1,28 @@
 $(document).ready(initializeApp);
 
 function initializeApp(){
-
     attachEventHandlers();
 }
 
 function attachEventHandlers(){
-    $('.movableElement').on('mousedown', movableElementMouseDown);
-    $('.movableElement, body').on('mouseup', movableElementMouseUp);
+    $('.movableElement').on('mousedown', mouseDown);
 
 }
 
-function mousePos(){
-    // console.log(event.pageX, event.pageY)
-    
-    return {x:event.pageX, y:event.pageY}
+function mouseDown(){
+    $('.moveBox').on('mousemove', moveMouse);
 }
 
-function movableElementMouseDown(){
-    var pointer;
-     $('.moveBox').on('mousemove', function(){
-         pointer = mousePos()
-         boxmove(pointer)
-     })
+function moveMouse(e){
+    var x = e.pageX + $('.movableElement').position().left;
+    var y = e.pageY + $('.movableElement').position().top;
+    console.log($('.movableElement').position().left, $('.movableElement').position().top);
 
-
+    $('.movableElement').offset({top: e.pageY, left: e.pageX});
+    $(window).on('mouseup', release);
 }
 
-function movableElementMouseUp(){
-    $('.moveBox').off('mousemove')
-}
-
-function boxmove(asdf){
-    console.log(asdf)
-
+function release(){
+    $('.moveBox').off('mousemove');
+    $(window).off('mouseup');
 }
